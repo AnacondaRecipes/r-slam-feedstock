f5836a2f7a3a36fb8bdd722c834d1961a77d6d91
Kai Tietz <ktietz@anaconda.com>
2019-05-10 16:17:34 +0200

    Updated .so to .dylib for osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,5 @@
 $R CMD INSTALL --build .
 # This rpath interferes with my attempt at allowing symbolic-link based interposition of Rblas.
 if [[ ${target_platform} == osx-64 ]]; then
-  ${INSTALL_NAME_TOOL} -delete_rpath ${PREFIX}/lib ${PREFIX}/lib/R/library/slam/libs/slam.so
+  ${INSTALL_NAME_TOOL} -delete_rpath ${PREFIX}/lib ${PREFIX}/lib/R/library/slam/libs/slam.dylib
 fi
